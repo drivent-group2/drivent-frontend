b363060ebfd0626e326958e99304ce7f85f281ac
Rodrigo <rodrigovf2102@gmail.com>
2022-12-20 10:12:56 -0300

    feat: payment required for seeing hotels page implemented

--- src/pages/Dashboard/Hotel/index.js
+++ src/pages/Dashboard/Hotel/index.js
@@ -1,3 +1,27 @@
+import styled from 'styled-components';
+import useTicket from '../../../hooks/api/useTicket';
+
 export default function Hotel() {
+  const { ticket, getTicket, ticketLoading } = useTicket();
+
+  if (ticketLoading) {
+    return 'Carregando....';
+  }
+  if (ticket.status === 'RESERVED') {
+    return (
+      <PaymentMessage>Você precisa ter confirmado pagamento antes de fazer a escolha da hospedagem</PaymentMessage>
+    );
+  }
   return 'Hotel: Em breve!';
 }
+
+const PaymentMessage = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: #8e8e8e;
+  font-size: 20px;
+  font-family: 'Roboto', sans-serif;
+`;
