f59bff79d9df8337439f774f5aa556ad34e4f49d
Lucas Tavares <l.tavaresc@hotmail.com>
2022-12-20 22:08:42 -0300

    feat: added an alert message on hotel page when user ticket type is remote

--- src/hooks/api/useTicket.js
+++ src/hooks/api/useTicket.js
@@ -5,7 +5,7 @@ import * as ticketApi from '../../services/ticketApi';
 export default function useTicket() {
   const token = useToken();
 
-  const { data: ticket, loading: ticketLoading, error: ticketError } = useAsync(() => ticketApi.getTicket(token), true);
+  const { data: ticket, loading: ticketLoading, error: ticketError } = useAsync(() => ticketApi.getTicket(token));
 
   return {
     ticket,
--- src/pages/Dashboard/Hotel/index.js
+++ src/pages/Dashboard/Hotel/index.js
@@ -1,3 +1,20 @@
+import styled from 'styled-components';
+import AlertMessage from '../../../components/Dashboard/common/AlertMessage';
+import useTicket from '../../../hooks/api/useTicket';
 export default function Hotel() {
-  return 'Hotel: Em breve!';
+  const { ticket, ticketLoading } = useTicket();
+
+  if (ticketLoading || !ticket) {
+    return <></>;
+    //TODO: ALERTS
+  }
+  if (ticket.TicketType?.isRemote ) {
+    return (
+      <AlertMessage>
+        Sua modalidade de ingresso não inclui hospedagem <br />
+        Prossiga para a escolha de atividades
+      </AlertMessage>
+    );
+  }
+  return <></>;
 }
