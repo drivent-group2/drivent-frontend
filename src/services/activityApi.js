293b9683305aea164147f52f1715b1f05798a8cf
Rodrigo <rodrigovf2102@gmail.com>
2023-01-03 17:59:55 -0300

    feat: activities page with activities vacances and activities aplications

--- src/services/activityApi.js
+++ src/services/activityApi.js
@@ -1,7 +1,19 @@
 import api from './api';
 
 export async function getActivities(token, ticketTypeId) {
-  const response = await api.get(`/activities/?ticketTypeId=${ticketTypeId}`,
+  const response = await api.get(`/activities/?ticketTypeId=${ticketTypeId}`, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+
+  return response.data;
+}
+
+export async function postTicketActivity(token, ticketId, activityId) {
+  const response = await api.post(
+    '/activities/enter',
+    { ticketId, activityId },
     {
       headers: {
         Authorization: `Bearer ${token}`,
